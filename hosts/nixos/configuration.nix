0120d8963f3471ef1a6e8886bdfd0042ecae5702
Arneke999 <merckxarne@gmail.com>
2026-07-04 21:08:03 +0200

    fixed configuration typo

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -10,7 +10,7 @@
       ./hardware-configuration.nix
     ];
 
-  nix.settings.experimental-features = [ "nix-commands" "flakes" ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
